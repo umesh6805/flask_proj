4be7b98bf22453b038579b319449f6a81a5333dd
umesh6805 <umeshkumar1947@gmail.com>
2026-06-24 19:00:45 +0530

    Added unit test

--- test/test_app.py
+++ test/test_app.py
@@ -1,6 +1,6 @@
 from app import app
 
-def test_home():
+def test_signup_page():
     client = app.test_client()
-    response = client.get("/")
+    response = client.get("/signup")
     assert response.status_code == 200
